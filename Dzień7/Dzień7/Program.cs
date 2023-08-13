6129559c4bede3d44ca5a0954f70b2521a1100cd
maniek maniek <km1265341@gmail.com>
2023-08-13 11:59:57 +0200

    Dzień 7 zadanie

---
+++ Dzień7/Dzień7/Program.cs
@@ -0,0 +1,26 @@
+﻿
+
+
+using Dzień7;
+
+Employee Employee1 = new Employee("Marek", "Nowak", 50);
+Employee Employee2 = new Employee("Jan", "Kochanowski", 27);
+Employee Employee3 = new Employee("Łukasz", "Kowalski", 57);
+
+Employee1.AddScore(9);
+Employee1.AddScore(9);
+Employee1.AddScore(9);
+Employee1.AddScore(9);
+Employee1.AddScore(9);
+
+Employee2.AddScore(4);
+Employee2.AddScore(5);
+Employee2.AddScore(6);
+Employee2.AddScore(7);
+Employee2.AddScore(8);
+
+Employee3.AddScore(5);
+Employee3.AddScore(6);
+Employee3.AddScore(7);
+Employee3.AddScore(8);
+Employee3.AddScore(9);
---
+++ Dzień7/TestProject1/UnitTest1.cs
@@ -0,0 +1,37 @@
+
+
+using Dzieñ7;
+
+namespace TestProject1
+{
+    public class Tests
+    {
+        
+
+        [Test]
+        public void TestEmployee1()
+        {
+            var employee = new Employee("Marek", "Nowak", 50);
+            employee.AddScore(7);
+            employee.AddScore(9);
+            employee.AddScore(-8);
+
+            var result = employee.Result;
+
+            Assert.AreEqual(8, result);
+        }
+
+        [Test]
+        public void TestEmployee2()
+        {
+            var employee = new Employee("Marek", "Nowak", 50);
+            employee.AddScore(7);
+            employee.AddScore(3);
+            employee.AddScore(-5);
+
+            var result = employee.Result;
+
+            Assert.AreEqual(5, result);
+        }
+    }
+}
